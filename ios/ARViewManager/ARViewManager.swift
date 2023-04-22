3e5dd9bd6e58ed3575b8e7c5a22bafbb164243cf
Charles <charles.m.long43@gmail.com>
2023-04-22 18:08:48 -0500

    experimental: got swift code to work with RN

---
+++ ios/ARViewManager/ARViewManager.swift
@@ -0,0 +1,8 @@
+//
+//  ARViewManager.swift
+//  ARLandNav
+//
+//  Created by Liming Li on 2023/4/22.
+//
+
+import Foundation
---
+++ ios/ARViewManager/ARViewManagerBridge.m
@@ -0,0 +1,8 @@
+//
+//  ARViewManagerBridge.m
+//  ARLandNav
+//
+//  Created by Liming Li on 2023/4/22.
+//
+
+#import <Foundation/Foundation.h>
