a99733c2d171a597d744cfd33e73ef49eb17ef45
Charles <charles.m.long43@gmail.com>
2023-04-29 22:35:30 -0500

    experimental: Tried implementing a text overlay but the checkpoint text still scales with distance.  cant figure out how to get the scale to be constant.  Its not a required feature though so figure it out later

--- ios/ARSpriteKit/ViewController.swift
+++ ios/ARSpriteKit/ViewController.swift
@@ -61,11 +61,20 @@ class ViewController: UIViewController, ARSKViewDelegate {
     
     func view(_ view: ARSKView, nodeFor anchor: ARAnchor) -> SKNode? {
         let ghostId = randomInt(min: 1, max: 6)
+                
+        let node1 = SKSpriteNode(imageNamed: "ghost\(ghostId)")
+        node1.name = "ghost"
         
-        let node = SKSpriteNode(imageNamed: "ghost\(ghostId)")
-        node.name = "ghost"
-        
-        return node
+      
+        let node2 = SKSpriteNode(imageNamed: "Checkpoint")
+        node2.position.x = 400
+        node2.position.y = 400
+        node2.name = "checkpoint-label-image"
+        node2.size = CGSize(width: 600, height: 600)
+        let parent = SKNode()
+        parent.addChild(node1)
+        parent.addChild(node2)
+      return parent
     }
     
     func session(_ session: ARSession, didFailWithError error: Error) {
@@ -82,4 +91,41 @@ class ViewController: UIViewController, ARSKViewDelegate {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
         
     }
+  
+//  func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+//    print("TESTING")
+//    guard let ghostNode = node.childNodes.first else {
+//                return
+//            }
+//
+//            // Get the distance from the camera to the ghost
+//            let cameraPosition = renderer.pointOfView?.simdPosition ?? simd_float3.zero
+//            let ghostPosition = ghostNode.simdPosition
+//            let distance = simd_distance(cameraPosition, ghostPosition)
+//
+//            // Set the ghost node's scale based on the distance
+//            let maxDistance: Float = 2.0 // Adjust as necessary
+//            let minScale: Float = 0.5 // Adjust as necessary
+//            let maxScale: Float = 1.0 // Adjust as necessary
+//            let scale = 1 - min(max(distance - 1, 0), maxDistance) / maxDistance * (maxScale - minScale) + minScale
+//            ghostNode.scale = SCNVector3(x: scale * 20, y: scale * 20, z: scale * 20)
+//      }
+  func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+    print("TESTING")
+    guard let ghostNode = node.childNodes.first else {
+                return
+            }
+
+            // Get the distance from the camera to the ghost
+            let cameraPosition = renderer.pointOfView?.simdPosition ?? simd_float3.zero
+            let ghostPosition = ghostNode.simdPosition
+            let distance = simd_distance(cameraPosition, ghostPosition)
+
+            // Set the ghost node's scale based on the distance
+            let maxDistance: Float = 2.0 // Adjust as necessary
+            let minScale: Float = 0.5 // Adjust as necessary
+            let maxScale: Float = 1.0 // Adjust as necessary
+            let scale = 1 - min(max(distance - 1, 0), maxDistance) / maxDistance * (maxScale - minScale) + minScale
+            ghostNode.scale = SCNVector3(x: scale * 20, y: scale * 20, z: scale * 20)
+      }
 }
